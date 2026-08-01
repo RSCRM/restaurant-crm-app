a0d243d83f15d5533e9ce47702a3b4fe7bb38bd8
catsocute <nguyenlenhat205@gmail.com>
2026-08-01 14:30:49 +0700

    nothing

--- src/app/routes/portal/booking/booking.component.ts
+++ src/app/routes/portal/booking/booking.component.ts
@@ -1,30 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Store } from '@ngrx/store';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Component } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { NzModalService } from 'ng-zorro-antd/modal';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzTagModule } from 'ng-zorro-antd/tag';
-import { STColumn, STComponent, STModule, STChange } from '@delon/abc/st';
 import { PageHeaderModule } from '@delon/abc/page-header';
-
-import { selectContextToken } from '../../auth/store/auth.selectors';
-import { BookingStatus, BookingResponse } from './booking.model';
-import { BookingService } from './booking.service';
-import { BookingFormComponent } from './booking-form/booking-form.component';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { I18nPipe } from '@delon/theme';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [NzTypographyModule, NzCardModule],
+  imports: [NzCardModule, PageHeaderModule, I18nPipe],
   templateUrl: './booking.component.html'
 })
 export class BookingComponent {}
